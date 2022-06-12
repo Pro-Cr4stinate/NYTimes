d5602690be4970826b5504f8f8a246922898d1b2
Pro-Cr4stinate <limitlessgamingid@yahoo.com>
2022-06-12 21:57:13 +0700

    more changes

--- pythonbackend.py
+++ pythonbackend.py
@@ -1,12 +1,21 @@
 import flask
 import pynytimes
 import os
-NYTarticles = pynytimes.NYTAPI("yVgnCb76kweCdxaPGcQcBPNNJo2yU8YH",parse_dates=True)
+import json
+
+NYTarticles = pynytimes.NYTAPI(os.environ.get('NYTIMES_KEY'),parse_dates=True)
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def index():
-    articles = NYTarticles.most_viewed(days=7)
-    name = "aksa"
-    return flask.render_template("index.html",name=name,articles=articles )
+
+    x = 0
+
+    starts = json.dumps(NYTarticles.most_viewed(days=1), default=str)
+    loads = json.loads(starts)
+    titles = loads[0]['title']
+    urls = loads[0]['url']
+    articles = loads[0]['abstract']
+    photos = loads[x]['media'][0]['media-metadata'][0]['url']
+    return flask.render_template("index.html",articles=articles, titles=titles, link=urls, images=photos)
